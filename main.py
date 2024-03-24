b97971a80054069f72776d516f9ace7b11b3467a
mostafa salari <83839313+m-salari@users.noreply.github.com>
2024-03-24 19:01:49 +0330

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,37 @@ class program:
         self.__my_server = Redis(host='localhost', port=port, db=0)
         self.__my_server.flushdb()
 
-    def main(self):
+    def run_price_updater(self):
         # task1
-        task1.price_updater_service(self.df, self.__my_server)
+        price_updater.price_updater_service(self.df, self.__my_server)
 
+    def run_performance_calculation(self):
         # task 2
-        # tsk2 = task2.stock_performance_calculation(self.__my_server)
-        # tsk2.run()
-
-        pass
+        tsk2 = stock_performance_calculation.stock_performance_calculation(self.__my_server)
+        tsk2.run()
 
 
-ttest = program()
-ttest.main()
+if __name__ == '__main__':
+    number_task = 0
+    while True:
+        try:
+            number_task = int(input('''
+            price_updater_service -- > 1
+            stock_performance_calculation --> 2
+            '''))
+
+            if not (number_task == 1 or number_task == 2):
+                raise ValueError('Please enter a valid number!!!')
+            break
+        except ValueError as e:
+            print(e)
+
+    app = program()
+    if number_task == 1:
+        app.run_price_updater()
+    else:
+        app.run_performance_calculation()
+
+
+
+
